bc3fb591c1001191db7f437e62ab1ec7d28d7e94
Arshad Khan <79917882+Arshadkhan-18@users.noreply.github.com>
2023-02-24 01:46:28 -0500

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and runn\n";
-  cout << "qwerty\n";
+  cout << "qwertyNNNN\n";
   return 0;
 }
